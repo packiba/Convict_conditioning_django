406af8603678c2414ac45741822fd1195bd84eac
packiba <packiba@gmail.com>
2024-11-11 22:10:33 +0200

    Add React as static files

--- convict_conditioning/urls.py
+++ convict_conditioning/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Временная главная страница
-def home(request):
-    return HttpResponse("Welcome to the Home Page")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/exercises/', include('exercises.urls')),
     path('api/journal/', include('workout_logs.urls')),
     path('api/auth/', include('users.urls')),
-    path('', home),  # Добавлен маршрут для главной страницы
+    path('', TemplateView.as_view(template_name="react/index.html")),
 ]
