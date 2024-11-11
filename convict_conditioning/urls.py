fe12679ba77a4c322235a228e05048c40d746cc7
packiba <packiba@gmail.com>
2024-11-11 21:52:40 +0200

    Добавлен маршрут для главной страницы

--- convict_conditioning/urls.py
+++ convict_conditioning/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+# Временная главная страница
+def home(request):
+    return HttpResponse("Welcome to the Home Page")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/exercises/', include('exercises.urls')),
     path('api/journal/', include('workout_logs.urls')),
     path('api/auth/', include('users.urls')),
+    path('', home),  # Добавлен маршрут для главной страницы
 ]
-
