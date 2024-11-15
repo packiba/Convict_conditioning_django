1bf1fc78f01f08350db4aa1508eaa9241dffaaa7
packiba <packiba@gmail.com>
2024-11-15 17:00:54 +0200

    fix 1

--- convict_conditioning/settings.py
+++ convict_conditioning/settings.py
@@ -137,10 +137,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'  
-]
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/react')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/react')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
