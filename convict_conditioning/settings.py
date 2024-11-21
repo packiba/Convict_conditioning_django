99a75c03d010b4b5526511049665e9fe098cd1e8
packiba <packiba@gmail.com>
2024-11-21 22:33:05 +0200

    fix_3

--- convict_conditioning/settings.py
+++ convict_conditioning/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/react')]
 
 # Default primary key field type
